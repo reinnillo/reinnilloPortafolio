303f4883ee38f1f3c6188dec6eb017c2efb5b673
reinnillo <todev30@outlook.com>
2024-06-24 23:40:57 -0500

    - le palique un poco mas de tamaño a los input del formulario. - agrege un script para aplicar una alerta para el sitio que aun estan en construccion.

---
+++ js/aviso.js
@@ -0,0 +1,4 @@
+const aviso = document.getElementById('aviso');
+aviso.addEventListener('click', () => {
+    alert('Hola! El sitio web aun esta en construccion, pero puedes visitar los otros o vuelve pronto. ☺');
+})
