929b7127d0511d419022653b79e23078bb329d3b
Alexander Reina P <todev30@outlook.com>
2024-09-07 09:47:19 -0500

    Update vite.config.js
    configuraciones para vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
-// Configuración básica de Vite
 export default defineConfig({
+  base: 'https://reinnillo.github.io/reinnilloPortafolio/',
   server: {
-    open: true, // Abre automáticamente el navegador al iniciar el servidor
+    open: true,
+    port: 3000, // Cambia el puerto del servidor de desarrollo si es necesario.
   },
-})
+  build: {
+    outDir: 'dist', // Directorio de salida para los archivos de producción.
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Personaliza cómo Vite divide el código en chunks.
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // Alias para simplificar importaciones.
+    },
+  },
+  plugins: [], // Añade plugins de Vite aquí si es necesario.
+});
+
